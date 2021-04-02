f0a8baa726f3ebb17e49ef2b3b80d9a999d5cc49
LAKSHAY SHARMA <lakshayne@gmail.com>
2021-04-02 18:36:57 +0530

    added encrytion in database

--- addphonenumberdb.js
+++ addphonenumberdb.js
@@ -1,15 +1,19 @@
 require('dotenv').config()
 const express=require('express')
 const app=express()
+const encrypt = require('mongoose-encryption');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}))
 const mongoose=require('mongoose')
 mongoose.connect('mongodb+srv://'+process.env.DBUSERNAME+':'+process.env.DBPASSWORD+'@cluster0.siopf.mongodb.net/'+process.env.DBNAME+'?retryWrites=true&w=majority',{ useNewUrlParser: true , useUnifiedTopology: true })
-const phoneSchema={
-    phone:String
-}
-const phonenos=mongoose.model('phoneno',phoneSchema)
 
+const phoneSchema=new mongoose.Schema({
+    phone:String
+})
+const secret=process.env.ENCRYPTPHONENO
+phoneSchema.plugin(encrypt, { secret: secret ,encryptedFields:['phone'] });
+
+const phonenos=mongoose.model('phoneno',phoneSchema)
 app.get('/done',(req,res)=>{res.send('SUBMITTED')})
 
 app.post('/add',(req,res)=>{
@@ -19,6 +23,12 @@ app.post('/add',(req,res)=>{
     newphoneno.save()
     console.log(req.body.phoneno);
     res.redirect('/done')
+})
+
+app.get('/see',(req,res)=>{
+    phonenos.find((err,phonenum)=>{
+        console.log(phonenum)
+    });
 })
 
 app.get('/',(req,res)=>res.sendFile(__dirname+'/phoneadd.html'))
